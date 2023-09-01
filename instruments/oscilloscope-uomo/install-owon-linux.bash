e1c04b763c057fd1ec9cbcd08162c13944f82f00
Johnny Cubides <jgcubidesc@gmail.com>
2023-08-31 21:42:48 -0500

    start install linux

---
+++ instruments/oscilloscope-uomo/install-owon-linux.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+#						 ↑
+# debug [-x -v]:[complete, abbreviated]
+# Brief:	Instalación de owon en Linux
+# Author: Johnny Cubides
+# e-mail: jgcubidesc@unal.edu.co
+# date: Thursday 31 August 2023
+status=$?
+
+
