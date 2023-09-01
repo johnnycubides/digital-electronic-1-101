43028d605dae7ee4a9f9da110e9b68684b8382f3
Johnny Cubides <jgcubidesc@gmail.com>
2023-08-31 21:55:47 -0500

    update docs

---
+++ instruments/oscilloscope-uomo/install-owon-linux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#						 ↑
+# debug [-x -v]:[complete, abbreviated]
+# Brief:	Instalación de owon en Linux
+# Author: Johnny Cubides
+# e-mail: jgcubidesc@unal.edu.co
+# date: Thursday 31 August 2023
+status=$?
+
+APP=OWON-VDS1022
+VERSION=1.1.5-cf19
+
+wget https://github.com/florentbr/OWON-VDS1022/archive/refs/tags/$APP-$VERSION.tar.gz
+tar xvf $APP-$VERSION.tar.gz
+cd $APP-$VERSION/ && sudo sh install-linux.sh
