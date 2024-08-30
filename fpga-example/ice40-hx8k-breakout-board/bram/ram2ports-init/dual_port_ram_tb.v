16a381542dead05aabc2a8ee789ce33e29499b7b
Johnny Cubides <jgcubidesc@gmail.com>
2024-08-29 21:50:40 -0500

    Update ram example

--- fpga-example/ice40-hx8k-breakout-board/bram/ram2ports-init/dual_port_ram_tb.v
+++ fpga-example/ice40-hx8k-breakout-board/bram/ram2ports-init/dual_port_ram_tb.v
@@ -1,38 +1,40 @@
+`include "./dual_port_ram.v"
+
 module dual_port_ram_tb;
 
-reg clock;
-reg write_enable_A;
-reg write_enable_B;
-reg [7:0] data_in_A;
-reg [7:0] data_in_B;
-reg [7:0] address_A;
-reg [7:0] address_B;
-wire [7:0] data_out_A;
-wire [7:0] data_out_B;
+  reg clock;
+  reg write_enable_A;
+  reg write_enable_B;
+  reg [7:0] data_in_A;
+  reg [7:0] data_in_B;
+  reg [7:0] address_A;
+  reg [7:0] address_B;
+  wire [7:0] data_out_A;
+  wire [7:0] data_out_B;
 
-// dual_port_ram #("./file.hex") dut (
-dual_port_ram dut (
-    .clock(clock),
-    .write_enable_A(write_enable_A),
-    .write_enable_B(write_enable_B),
-    .data_in_A(data_in_A),
-    .data_in_B(data_in_B),
-    .address_A(address_A),
-    .address_B(address_B),
-    .data_out_A(data_out_A),
-    .data_out_B(data_out_B)
-);
+  // dual_port_ram #("./file.hex") dut (
+  dual_port_ram dut (
+      .clock(clock),
+      .write_enable_A(write_enable_A),
+      .write_enable_B(write_enable_B),
+      .data_in_A(data_in_A),
+      .data_in_B(data_in_B),
+      .address_A(address_A),
+      .address_B(address_B),
+      .data_out_A(data_out_A),
+      .data_out_B(data_out_B)
+  );
 
-initial begin
+  initial begin
     clock = 0;
     write_enable_A = 0;
     write_enable_B = 0;
 
     #10;
-    address_A = 4'h0;
+    address_A = 4'h3;
 
     #10;
-    address_A = 4'h1;
+    address_A = 4'h4;
 
     // write_enable_A = 1;
     // write_enable_B = 1;
@@ -67,19 +69,18 @@ initial begin
 
     #10;
     $finish;
-end
+  end
 
-always begin
+  always begin
     clock = 0;
     #5;
     clock = 1;
     #5;
-end
+  end
 
-	// WAVES IN VCD TO OPEN IN GTKWAVE
-  initial
-  begin
-    $dumpfile("top.vcd");
+  // WAVES IN VCD TO OPEN IN GTKWAVE
+  initial begin
+    // $dumpfile("top.vcd");
     $dumpvars(0, dual_port_ram_tb);
   end
 
