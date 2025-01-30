7edd85b8a81bc2b982aaf44fbed130014bfc5448
Johnny Cubides <jgcubidesc@gmail.com>
2025-01-30 17:12:51 -0500

    update freqdiv example

---
+++ fpga-example/ice40-hx8k-breakout-board/blink-divfreq/top.v
@@ -0,0 +1,23 @@
+`include "./freqDiv.v"
+
+module top (
+    // 12MHz clock input
+    input  clk,
+    // Led outputs
+    output led
+);
+
+  freqDiv #(
+`ifdef SIM  // Macros de presíntesis
+      .FREQ_IN (10),    // 10 Tikcs
+      .FREQ_OUT(1)      // 1 Tikcs
+`else
+      .FREQ_IN (12e6),  // 12 MHz
+      .FREQ_OUT(1)      // 1 Hz
+`endif
+  ) frequencyDivider (
+      .CLK_IN (clk),
+      .CLK_OUT(led)
+  );
+
+endmodule
