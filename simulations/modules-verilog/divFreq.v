6731d533376019999121695f51539b52e09ab668
Johnny Cubides <jgcubidesc@gmail.com>
2025-02-17 17:51:42 -0500

    update

---
+++ simulations/modules-verilog/divFreq.v
@@ -0,0 +1,28 @@
+// filename: divFreq.v
+module divFreq #(
+    parameter integer FREQ_IN = 1000,
+    parameter integer FREQ_OUT = 100,
+    parameter integer INIT = 0
+) (
+    // Inputs and output ports
+    input CLK_IN,
+    output reg CLK_OUT = 0
+);
+
+  localparam integer COUNT = (FREQ_IN / FREQ_OUT) / 2;
+  localparam integer SIZE = $clog2(COUNT);
+  localparam integer LIMIT = COUNT - 1;
+
+  // Declaración de señales [reg, wire]
+  reg [SIZE-1:0] count = INIT;
+
+  // Descripción del comportamiento
+  always @(posedge CLK_IN) begin
+    if (count == LIMIT) begin
+      count   <= 0;
+      CLK_OUT <= ~CLK_OUT;
+    end else begin
+      count <= count + 1;
+    end
+  end
+endmodule
