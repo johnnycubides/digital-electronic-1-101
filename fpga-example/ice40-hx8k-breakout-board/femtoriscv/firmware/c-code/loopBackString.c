0f08e6784459778c8e448984a65fa435a236a481
Johnny Cubides <jgcubidesc@gmail.com>
2025-06-14 22:33:16 -0500

    update

---
+++ fpga-example/ice40-hx8k-breakout-board/femtoriscv/firmware/c-code/loopBackString.c
@@ -0,0 +1,23 @@
+#include "./libs/time.h"
+#include "./libs/uart.h"
+#include <stdint.h>
+
+#define NULL (void *)0
+
+#define IO_BASE 0x400000
+#define IO_LEDS 4
+
+volatile uint32_t *const gp = (uint32_t *)IO_BASE;
+
+char buffer[16] = "echo\n\r";
+
+int main() {
+  putstring(buffer);
+  wait(20);
+  while (1) {
+    wait(10);
+    putchar(buffer);
+    wait(10);
+  }
+  return 0;
+}
