830b7b6abd787cb750384210cb43e1963230de97
Johnny Cubides <jgcubidesc@gmail.com>
2026-02-08 16:41:23 -0500

    update

--- fpga-example/altera-c4e6e10/bram/ram_counter/verilog_sources.tcl
+++ fpga-example/altera-c4e6e10/bram/ram_counter/verilog_sources.tcl
@@ -1,2 +1,5 @@
 # Archivo generado automáticamente - Lista de archivos Verilog
 set_global_assignment -name VERILOG_FILE top.v
+set_global_assignment -name VERILOG_FILE clk10hz.v
+set_global_assignment -name VERILOG_FILE counter8bits.v
+set_global_assignment -name VERILOG_FILE dual_port_ram.v
