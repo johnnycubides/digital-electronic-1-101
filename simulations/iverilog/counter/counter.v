690b901c36dee1079477e886c46ea85a68bc20a3
Johnny Cubides <jgcubidesc@gmail.com>
2026-02-14 21:00:49 -0500

    resolve type and paramod in json with sed for netlistsvg

--- simulations/iverilog/counter/counter.v
+++ simulations/iverilog/counter/counter.v
@@ -12,8 +12,12 @@ module counter (
   reg [WIDTH-1 : 0] out;
   wire clk, reset;
 
-  always @(posedge clk or posedge reset)
-    if (reset) out <= 0;
-    else out <= out + 1'd1;
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      out <= 0;
+    end else begin
+      out <= out + 1'd1;
+    end
+  end
 
 endmodule  // counter
